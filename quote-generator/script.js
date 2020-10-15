10c20a4d16177a90f95fc9f2da1ae3be62ae40e9
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-14 22:55:35 -0400

    Implemented quote fetch functionality from api

--- quote-generator/script.js
+++ quote-generator/script.js
@@ -1,6 +1,13 @@
+// SELECTORS
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 // Base url for quotes api
-const baseUrl = `https://api.quotable.io`;
 // quotes api documentation available at https://github.com/lukePeavey/quotable
+const baseUrl = `https://api.quotable.io`;
 
 // FUNCTIONS
 // fetch quote from api
@@ -8,6 +15,17 @@ getQuote = async () => {
     try {
         const response = await fetch(`${baseUrl}/random`);
         const data = await response.json();
+        console.log(data);
+
+        // Display quote & author texts in the DOM
+        authorText.innerText = data.author;
+        quoteText.innerText = data.content;
+
+        if (data.content.length > 120) {
+            quoteText.classList.add("long-quote");
+        } else {
+            quoteText.classList.remove("long-quote");
+        }
     } catch (error) {
         getQuote();
         console.log("Something went wrong :(", error);
