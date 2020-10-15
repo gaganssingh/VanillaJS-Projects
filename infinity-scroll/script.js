8d7fda870ce3574d51441b18f527ac31c519ee2b
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-15 00:58:55 -0400

    Added infinite scroll and fetch new images functionality

--- infinity-scroll/script.js
+++ infinity-scroll/script.js
@@ -2,7 +2,15 @@
 const imageContainer = document.getElementById("image-container");
 const loadingSpinner = document.getElementById("loader");
 
+// STATE
 let photosArray = [];
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let pageNumber = 1;
+
+const limit = 30;
+const imageUrl = `https://picsum.photos/v2/list`;
 
 // FUNCTIONS
 // Helper function
@@ -12,8 +20,19 @@ setAttributes = (el, attributes) => {
     }
 };
 
+// Helper Function
+imageLoaded = () => {
+    imagesLoaded++;
+    if (imagesLoaded === totalImages) {
+        ready = true;
+        loader.hidden = true;
+    }
+};
+
 // Add photos to DOM
 showPhotosInDOM = () => {
+    imagesLoaded = 0;
+    totalImages = photosArray.length;
     photosArray.forEach((photo) => {
         // Create <a> element
         const item = document.createElement("a");
@@ -33,6 +52,9 @@ showPhotosInDOM = () => {
             title: photo.author,
         });
 
+        // Check when loading has finished
+        img.addEventListener("load", imageLoaded);
+
         // Put <img> inside <a> element -> Then <a> inside image container
         item.appendChild(img);
         imageContainer.appendChild(item);
@@ -40,18 +62,29 @@ showPhotosInDOM = () => {
 };
 
 // FLorem Picsum API
-fetchPhotos = async () => {
-    const limit = 10;
-    const imageUrl = `https://picsum.photos/v2/list/?limit=${limit}`;
-
+fetchPhotos = async (pageNumber = 1) => {
     try {
-        const response = await fetch(imageUrl);
+        const response = await fetch(
+            `${imageUrl}/?limit=${limit}&page=${pageNumber}`
+        );
         photosArray = await response.json();
-        console.log(photosArray);
         showPhotosInDOM();
     } catch (error) {
         console.log(error);
     }
 };
+
+// If user scrolled to the bottom, fetch more images from
+window.addEventListener("scroll", () => {
+    if (
+        window.innerHeight + window.scrollY >=
+            document.body.offsetHeight - 1000 &&
+        ready
+    ) {
+        ready = false;
+        pageNumber++;
+        fetchPhotos(pageNumber);
+    }
+});
 
 fetchPhotos();
