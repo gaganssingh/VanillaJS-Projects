0c0b97ea1b9244a299020f38aa76e05ad6ae06c4
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-15 21:41:29 -0400

    Added theme preference storage to local storage

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -33,12 +33,25 @@ lightMode = () => {
 switchTheme = (event) => {
     if (event.target.checked) {
         document.documentElement.setAttribute("data-theme", "dark");
+        localStorage.setItem("theme", "dark");
         darkMode();
     } else {
         document.documentElement.setAttribute("data-theme", "light");
+        localStorage.setItem("theme", "light");
         lightMode();
     }
 };
 
 // EVENT LISTENERS
 toggleSwitch.addEventListener("change", switchTheme);
+
+// Check if theme preference is stored in local storage
+const currentTheme = localStorage.getItem("theme");
+if (currentTheme) {
+    document.documentElement.setAttribute("data-theme", currentTheme);
+
+    if (currentTheme === "dark") {
+        toggleSwitch.checked = true;
+        darkMode();
+    }
+}
