9963be9ad773e8837c2960502f7770a1522fe8be
Gagan Singh <gagansingh.dev@outlook.com>
2020-10-16 14:45:54 -0400

    Refactored to separate JS code in it's own file

---
+++ animatied-landing-page/script.js
@@ -0,0 +1,21 @@
+// INIT ANIMATE ON SCROLL LIBRARY
+// Default AOS settings
+AOS.init({
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 200, // values from 0 to 3000, with step 50ms
+    duration: 1500, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom",
+});
+
+// Hamburger navigation for mobile
+function toggleNavbar() {
+    document
+        .getElementById("example-collapse-navbar")
+        .classList.toggle("hidden");
+    document
+        .getElementById("example-collapse-navbar")
+        .classList.toggle("block");
+}
